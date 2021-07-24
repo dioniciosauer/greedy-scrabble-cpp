a7d9a0d349ba40e6ce9bf48c4c162d0de1ba3ddf
dionmesa <dsauer@mesaphotonics.com>
2021-07-24 10:40:25 -0600

    TrieNode now complete, compiling

--- include/TrieNode.hpp
+++ include/TrieNode.hpp
@@ -1,7 +1,6 @@
 #ifndef TRIENODE_HPP
 #define TRIENODE_HPP
 
-
 #include <map>
 #include <memory>
 #include <optional>
@@ -15,36 +14,34 @@ namespace Scrabble
         public:
             TrieNode(const std::vector<std::string>& words);
 
+            bool ContainsPrefix(const std::string& word) const;
+
+            bool ContainsWord(const std::string& word) const;
+
+            std::optional<std::shared_ptr<const TrieNode>> FindNodeWithPrefix(const std::string& prefix) const;
+
+            const std::vector<char> ValidFirstLetters(const std::string& word) const;
+
+            const std::vector<char> ValidLastLetters(const std::string& word) const;
+
             bool leaf() const;
 
             bool terminal() const;
-
-            const std::map<char, std::shared_ptr<TrieNode>>& children() const;
-
-            const std::vector<char>& ValidLastLetters(const std::string& word);
-
-            const std::vector<char>& ValidFirstLetter(const std::string& prefix);
-
-            std::optional<std::shared_ptr<const TrieNode>> GetNodeWithPrefix(const std::string& prefix) const;
-
-            bool ContainsPrefix(const std::string& word);
-
-            bool ContainsWord(const std::string& word) const;
 
         private:
             TrieNode(const std::vector<std::string>& words, const int level, const std::string& prefix);
 
             void CreateChildren();
 
-            bool leaf;
-            bool terminal;
+            bool leaf_;
+            bool terminal_;
             int  level;
             std::vector<std::string> words;
             std::string prefix;
             std::vector<std::vector<std::shared_ptr<TrieNode>>> levels;
             std::map<char, std::shared_ptr<TrieNode>> children;
         
-    }
+    };
 }
 
 #endif TRIENODE_HPP
--- source/TrieNode.cpp
+++ source/TrieNode.cpp
@@ -6,19 +6,27 @@
 using namespace Scrabble;
 
 TrieNode::TrieNode(const std::vector<std::string>& words) 
-: leaf(false), terminal(false), level(0), prefix(""), words(words), children()
+: leaf_(false), terminal_(false), level(0), prefix(""), words(words), children()
 {
+    /* Sort at the root and it shouldn't be necessary to sort again due to
+     * how the child creation process works
+     */
     std::sort(this->words.begin(), this->words.end());
-    CreateChildren();
+    if(words.size() != 0)
+    {
+        CreateChildren();
+    }
 }
 
-TrieNode::TrieNode(const std::vector<std::string>& words, int level, const std::string& prefix) 
-: level(level), prefix(prefix), words(words), children()
+TrieNode::TrieNode(const std::vector<std::string>& words, const int level, const std::string& prefix) 
+: level(level), prefix(prefix), words(words), children(), terminal_(std::find(words.begin(), words.end(), prefix) != words.end())
 {
-    leaf = words.size() == 1 && level == words.front().length();
-    terminal = std::find(words.begin(), words.end(), prefix) != words.end();
-    if(!leaf)
+    // A node is a terminal node if its prefix exists as a word in its collection of words
+    // A node is a leaf if it is terminal, and its prefix is the only word in its collection of words
+    leaf_ = words.size() == 1 && terminal_;
+    if(!leaf_)
     {
+        // Naturally, children only need to be made for non-leaf nodes.
         CreateChildren();
     }
 }
@@ -31,12 +39,21 @@ void TrieNode::CreateChildren()
 
     if(words.size() == 1)
     {
+        // If there's only one word, then only one child needs to be made for this node.
         const auto firstLetter{ words.front().front() };
-        children.try_emplace(firstLetter, std::make_shared<TrieNode>(words, childLevel, prefix + firstLetter));
+        children.try_emplace(firstLetter, std::shared_ptr<TrieNode>(new TrieNode(words, childLevel, prefix + firstLetter)));
     }
     else
     {
         std::vector<std::string> childWords{ };
+        /* The potential prefixes for the child nodes are the parents prefix with each letter
+         * of the alphabet appended. Create each potential child prefix from the alphabet,
+         * and iterate through the parents words to see if there's a word with that prefix.
+         * 
+         * If so, collect it. After iterating through every word, if the list of child words
+         * is not empty, initialize a new child with those words, storing it in the children
+         * of the parent with the letter of the alphabet it corresponds to.
+         */
         for(const auto letter : alphabet)
         {
             for(auto word{ words.begin() }; word != words.end(); word = std::next(word))
@@ -48,7 +65,7 @@ void TrieNode::CreateChildren()
                 }
                 else if(std::next(word) == words.end() && !childWords.empty())
                 {
-                    children.try_emplace(letter, std::make_shared<TrieNode>(childWords, childLevel, childPrefix));
+                    children.try_emplace(letter, std::shared_ptr<TrieNode>(new TrieNode(childWords, childLevel, childPrefix)));
                     childWords.clear();
                 }
             }
@@ -56,17 +73,38 @@ void TrieNode::CreateChildren()
     }
 }
 
-std::optional<std::shared_ptr<const TrieNode>> TrieNode::GetNodeWithPrefix(const std::string& prefix) const
+bool TrieNode::ContainsPrefix(const std::string& word) const
+{
+    return FindNodeWithPrefix(prefix).has_value();
+}
+
+bool TrieNode::ContainsWord(const std::string& word) const
+{
+    if(const auto wordAsPrefix{ FindNodeWithPrefix(prefix) }; wordAsPrefix)
+    {
+        return wordAsPrefix.value()->terminal_;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+std::optional<std::shared_ptr<const TrieNode>> TrieNode::FindNodeWithPrefix(const std::string& prefix) const
 {
     if(prefix.length() == level)
     {
-        return this->prefix == prefix ? { shared_from_this() } : { };
+        return this->prefix == prefix ? std::optional<std::shared_ptr<const TrieNode>>{ shared_from_this() } :  std::optional<std::shared_ptr<const TrieNode>>{ };
     }
     else if(prefix.length() < level)
     {
         if(auto child{ children.find(prefix.at(level)) }; child != children.end())
         {
-            return child->second->GetNodeWithPrefix(prefix);
+            return child->second->FindNodeWithPrefix(prefix);
+        }
+        else
+        {
+            return { };
         }
     }
     else
@@ -75,22 +113,51 @@ std::optional<std::shared_ptr<const TrieNode>> TrieNode::GetNodeWithPrefix(const
     }
 }
 
-bool TrieNode::ContainsWord(const std::string& word) const
+const std::vector<char> TrieNode::ValidFirstLetters(const std::string& word) const
 {
-    
+    std::vector<char> validLetters{ };
+
+    for(const auto& [letter, child] : children)
+    {
+        if(ContainsWord(letter + word))
+        {
+            validLetters.push_back(letter);
+        }
+    }
+    return validLetters;
 }
 
-const std::map<char, std::shared_ptr<TrieNode>>& TrieNode::children() const
+const std::vector<char> TrieNode::ValidLastLetters(const std::string& word) const
 {
-    return children;
+    std::vector<char> validLetters{ };
+
+    if(auto childExists{ FindNodeWithPrefix(word) }; childExists)
+    {
+        const auto child{ childExists.value() };
+        if(child->leaf_)
+        {
+            return validLetters;
+        }
+        else
+        {
+            for(const auto [letter, grandChild] : child->children)
+            {
+                if(grandChild->terminal_)
+                {
+                    validLetters.push_back(letter);
+                }
+            }
+        }
+    }
+    return validLetters;
 }
 
 bool TrieNode::leaf() const
 {
-    return leaf;
+    return leaf_;
 }
 
 bool TrieNode::terminal() const
 {
-    return terminal;
+    return terminal_;
 }
